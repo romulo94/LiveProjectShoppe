c63ccce06b5c64a46b9fb68281f664123b1c5739
Romulo <romulorocha063@gmail.com>
2022-06-22 19:53:48 -0300

    feat: start final live

--- packages/util-ui/deploy.sh
+++ packages/util-ui/deploy.sh
@@ -9,3 +9,4 @@ cat ./import-map.json | ./jq --arg NEW_URL "$NEW_URL" '.imports["@shoppe/util-ui
 aws s3 cp dist s3://mfe-shoppe/config/mfe/util-ui/$VERSION --recursive
 aws s3 cp new.importmap.json s3://mfe-shoppe/config/import-map.json
 aws cloudfront create-invalidation --distribution-id E2CSYO6LJ44WFR --paths '/config/import-map.json'
+aws cloudfront create-invalidation --distribution-id E2CSYO6LJ44WFR --paths '/index.html'
